fea4c3b0bacdec64b3e5332843dac89a011375c6
Ross Byrne <ross.byrne@outlook.ie>
2017-08-18 15:02:11 +0100

    Connects to local ftp server and lists files

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 public class Main extends Application {
 
@@ -22,8 +29,70 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
+
+
+        ftpTest();
+
         launch(args);
 
     } // main()
+
+    public static void ftpTest() {
+
+        String server = "localhost";
+
+        FTPClient client = new FTPClient();
+
+        try {
+            InetAddress addr = InetAddress.getByName(server);
+            client.connect(addr);
+            client.login("bob", "qwerty");
+
+            if (client.isConnected()) {
+
+                // enter passive mode
+                client.enterLocalPassiveMode();
+                //client.enterRemoteActiveMode(addr,3000);
+
+                System.out.println("Connected");
+
+                System.out.print(client.getReplyString());
+                FTPFile[] files = client.listFiles();
+
+                System.out.println("No of Files: " + files.length);
+
+                // Obtain a list of filenames in the current working
+                // directory. When no file found an empty array will
+                // be returned.
+                String[] names = client.listNames();
+
+                //System.out.println("No of files: " + names.length);
+                for (String name : names) {
+                    System.out.println("Name = " + name);
+                }
+
+                FTPFile[] ftpFiles = client.listFiles();
+                for (FTPFile ftpFile : ftpFiles) {
+                    // Check if FTPFile is a regular file
+                    if (ftpFile.getType() == FTPFile.FILE_TYPE) {
+                        System.out.printf("FTPFile: %s; %s; %s%n",
+                                ftpFile.getName(),
+                                FileUtils.byteCountToDisplaySize(ftpFile.getSize()),
+                                ftpFile.getTimestamp().getTime().toString());
+                    }
+                }
+            }
+            client.logout();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error Disconnecting");
+            }
+        }
+    }
 
 } // class
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -13,45 +13,12 @@ public class MainController implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
 
+        // run the ftp test
+       // ftpTest();
+
     } // initialize()
 
-    public void ftpTest() {
-        FTPClient client = new FTPClient();
 
-        try {
-            client.connect("ftp.example.org");
-            client.login("admin", "admin123**");
-
-            if (client.isConnected()) {
-                // Obtain a list of filenames in the current working
-                // directory. When no file found an empty array will
-                // be returned.
-                String[] names = client.listNames();
-                for (String name : names) {
-                    System.out.println("Name = " + name);
-                }
-
-                FTPFile[] ftpFiles = client.listFiles();
-                for (FTPFile ftpFile : ftpFiles) {
-                    // Check if FTPFile is a regular file
-                    if (ftpFile.getType() == FTPFile.FILE_TYPE) {
-                        System.out.printf("FTPFile: %s; %s%n",
-                                ftpFile.getName(),
-                                FileUtils.byteCountToDisplaySize(ftpFile.getSize()));
-                    }
-                }
-            }
-            client.logout();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                client.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
 
 } // class
