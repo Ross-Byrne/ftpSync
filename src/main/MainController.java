adcea1efce182fdee3f01fc41b5502db9d6b29c8
Ross Byrne <ross.byrne@outlook.ie>
2017-08-18 14:15:56 +0100

    Updated pom

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,16 +1,57 @@
 package main;
 
 import javafx.fxml.Initializable;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     } // initialize()
+
+    public void ftpTest() {
+        FTPClient client = new FTPClient();
+
+        try {
+            client.connect("ftp.example.org");
+            client.login("admin", "admin123**");
+
+            if (client.isConnected()) {
+                // Obtain a list of filenames in the current working
+                // directory. When no file found an empty array will
+                // be returned.
+                String[] names = client.listNames();
+                for (String name : names) {
+                    System.out.println("Name = " + name);
+                }
+
+                FTPFile[] ftpFiles = client.listFiles();
+                for (FTPFile ftpFile : ftpFiles) {
+                    // Check if FTPFile is a regular file
+                    if (ftpFile.getType() == FTPFile.FILE_TYPE) {
+                        System.out.printf("FTPFile: %s; %s%n",
+                                ftpFile.getName(),
+                                FileUtils.byteCountToDisplaySize(ftpFile.getSize()));
+                    }
+                }
+            }
+            client.logout();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 } // class
