ac005f0f155b067a46158050b6cbe9f14e178602
Ross Byrne <ross.byrne@outlook.ie>
2017-08-18 13:53:46 +0100

    setting up project

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("ftp Sync");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMinWidth(600);
+        primaryStage.setMinHeight(600);
+        primaryStage.show();
+
+    } // start()
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    } // main()
+
+} // class
