95d781dad21529255e79167755f8615616eb93e9
Ross Byrne <ross.byrne@outlook.ie>
2017-08-19 20:38:10 +0100

    Can now login to ftp server and all files in root directory are displayed

--- src/main/java/application/MainController.java
+++ src/main/java/application/MainController.java
@@ -9,6 +9,7 @@ import javafx.scene.image.ImageView;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +25,8 @@ public class MainController implements Initializable {
     @FXML TextField addressTF;
     @FXML TextField usernameTF;
     @FXML PasswordField passwordPF;
+    @FXML Button loginBT;
+    @FXML Label messageLB;
 
     // variables
 
@@ -45,9 +48,44 @@ public class MainController implements Initializable {
         fileTreeView.setRoot(rootItem);
 
 
-       ftpTest();
+       //ftpTest();
     } // initialize()
 
+    // onClick method for login button
+    @FXML void loginButtonClick_OnAction(){
+
+        System.out.println("Login Click");
+
+        // clear message label
+        messageLB.setText("");
+
+        // check that server address is entered
+        // check that username and password are entered
+
+        if(addressTF.getCharacters().length() < 3){
+
+            this.messageLB.setText("Error, enter ftp server address.");
+            return;
+        }
+
+        if(usernameTF.getCharacters().length() < 1){
+
+            this.messageLB.setText("Error, enter Username.");
+            return;
+        }
+
+        if(passwordPF.getCharacters().length() < 1){
+
+            this.messageLB.setText("Error, enter Password.");
+            return;
+        }
+
+        System.out.println("Logging in with Username: " + usernameTF.getText() + " and Password: " + passwordPF.getText());
+
+        // try login
+        connectToServer(this.addressTF.getText(), this.usernameTF.getText(), this.passwordPF.getText());
+
+    } // loginButtonClick()
 
     private void getFiles(TreeItem dir){
 
@@ -72,33 +110,59 @@ public class MainController implements Initializable {
 
     } // addFileToDir()
 
-    private void connectToServer(InetAddress address){
+    private void connectToServer(String serverAddress, String username, String password){
 
         // try connect
         if(this.client.isConnected() == false){
 
             try {
 
+                // create a server address
+                this.address = InetAddress.getByName(serverAddress);
 
+                // connect to the address
                 client.connect(address);
-                client.login("bob", "qwerty");
+
+                // try and login
+                client.login(username, password);
 
                 if (client.isConnected()) {
+
+                    System.out.print(client.getReplyString());
+
+                    if (!FTPReply.isPositiveCompletion(client.getReplyCode())){
+
+                        messageLB.setText("Error: " + client.getReplyString());
+
+                        client.disconnect();
+
+                        return;
+                    } // if
 
                     // enter passive mode
                     client.enterLocalPassiveMode();
 
-                    System.out.println("Connected");
-                    System.out.print(client.getReplyString());
+                    // logged in ok
+                    messageLB.setText(client.getReplyString());
 
-                } else {
+                    // get files
 
-                    System.out.println("");
-                }
+                    // Obtain a list of filenames in the current working
+                    // directory. When no file found an empty array will
+                    // be returned.
+                    String[] names = client.listNames();
+
+                    //System.out.println("No of files: " + names.length);
+                    for (String name : names) {
+                        System.out.println("Name = " + name);
+                        fileTreeView.getRoot().getChildren().add(new TreeItem<>(name));
+                    }
+                } // if
 
             }catch (Exception e){
 
                 System.out.println("Error: " + e.getMessage());
+                messageLB.setText("Error: " + e.getMessage());
 
             } finally {
 
